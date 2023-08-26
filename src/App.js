3c2f14c7e02db189ef6128e942d65fcc69b23838
Barsbekteam <barsbekbarsbek99@gmail.com>
2023-08-26 15:55:32 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import June from "./components/June";
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="all container">
+      <div className="mounth">
+          <h1>Май</h1>
+          <h1>Июнь</h1>
+          <h1>Июль</h1>
+          <h1>Авг.</h1>
+          <h1>Сент.</h1>
+          <h1>Окт.</h1>
+          <h1>Нояб.</h1>
+          <h1>Дек.</h1>
+          <h1>Янв.</h1>
+          <h1>Февр.</h1>
+          <h1>Март</h1>
+          <h1>Апр.</h1>
+          <h1>Май</h1>
+      </div>
+        <div className="calendar">
+            <div className="day">
+                <h1>Пн</h1>
+                <h1>Ср</h1>
+                <h1>Пт</h1>
+            </div>
+            <June/>
+        </div>
+        <div className="small">
+            Меньше
+            <button></button>
+            <button style={{background: '#ACD5F2'}}></button>
+            <button style={{background: '#7FA8C9'}}></button>
+            <button style={{background: '#527BA0'}}></button>
+            <button style={{background: '#254E77'}}></button>
+            <span style={{marginLeft: '5px'}}>Больше</span>
+        </div>
     </div>
   );
 }
---
+++ src/components/June/index.js
@@ -0,0 +1,60 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+
+const June = () => {
+    const [fetchData, setFetchData] = useState({});
+    const [dateCount, setDateCount] = useState([])
+    const getData = async () => {
+        try {
+            const response = await axios.get('https://dpg.gg/test/calendar.json');
+            const{data} = await response
+            const dataArray = Object.entries(data).map(([date, count]) => ({ date, count }))
+            setDateCount(dataArray)
+            setFetchData(data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+    console.log(dateCount.slice(0, 1).map(el => el.date))
+    const renderCalendar = () => {
+        const startDate = new Date(dateCount.slice(0, 1).map(el => el.date));
+        const endDate = new Date(dateCount.slice(-1).map(el => el.date));
+        const calendar = [];
+        let currentDate = new Date(startDate);
+        while (currentDate <= endDate) {
+            const dateString = currentDate.toISOString().slice(0, 10); // Convert date to YYYY-MM-DD format
+            const count = fetchData[dateString] || 0; // Get count for the date, default to 0 if not found
+
+            calendar.push(
+                    <div key={dateString}  className="calendar-day">
+                        <button className="calendar_btn" style={{
+                            background: count === 0 ? 'transparent' : count > 0 && count <10 ? '#ACD5F2'
+                                : count > 9 && count < 20 ? '#7FA8C9' :
+                                    count > 19 && count < 30 ? '#527BA0'
+                                   : '#254E77',
+                        }}> <span style={{display: 'none'}}>{dateString.slice(-2)}{count}</span></button>
+
+                    </div>
+            );
+
+            currentDate.setDate(currentDate.getDate() + 1); // Move to the next day
+        }
+
+        return calendar;
+    };
+
+    return (
+        <div className="data" >
+            <button style={{
+                margin: '6px 1px 1px 1px',
+            }}></button>
+            {renderCalendar()}
+        </div>
+    );
+};
+
+export default June;
