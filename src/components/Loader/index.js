41c53e6285a0aa7e4c7dff9ac0e8f5b2fc22700f
Barsbekteam <99112399+Barsbekteam@users.noreply.github.com>
2023-08-26 13:31:45 +0300

    added loader (#1)

---
+++ src/components/Loader/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.scss'
+const Loader = () => {
+    return (
+        <div className='loader'>
+            <div className='spinny-thing'/>
+        </div>
+    );
+};
+
+export default Loader;
